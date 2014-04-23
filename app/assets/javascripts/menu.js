f954fa5106ee26f17bf0548bf65cb519db0f0d4d
Michael Lannin <mlannin23@gmail.com>
2014-04-23 00:29:32 -0700

    Add side menu

---
+++ app/assets/javascripts/menu.js
@@ -0,0 +1,31 @@
+var layout   = document.getElementById('layout'),
+    menu     = document.getElementById('menu'),
+    menuLink = document.getElementById('menu-link');
+
+function toggleClass(element, className) {
+    var classes = element.className.split(/\s+/),
+        length = classes.length,
+        i = 0;
+
+    for(; i < length; i++) {
+      if (classes[i] === className) {
+        classes.splice(i, 1);
+        break;
+      }
+    }
+    // The className is not found
+    if (length === classes.length) {
+        classes.push(className);
+    }
+
+    element.className = classes.join(' ');
+}
+
+menuLink.onclick = function (e) {
+    var active = 'active';
+
+    e.preventDefault();
+    toggleClass(layout, active);
+    toggleClass(menu, active);
+    toggleClass(menuLink, active);
+};
