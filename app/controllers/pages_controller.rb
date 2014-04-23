0719434ddc2329a8b7764c21dbb48913f004c4c3
Michael Lannin <mlannin23@gmail.com>
2014-04-23 00:01:46 -0700

    Add pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def schedule
+  end
+
+  def assignments
+  end
+
+  def syllabus
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'pages#home'
+  get 'schedule', to: 'pages#schedule'
+  get 'assignments', to: 'pages#assignments'
+  get 'syllabus', to: 'pages#syllabus'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
