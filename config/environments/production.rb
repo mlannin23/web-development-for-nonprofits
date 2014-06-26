f8e4771bd7a04887f920a2f50a4bacaf270a6717
Michael Lannin <mlannin23@gmail.com>
2014-06-25 20:54:45 -0700

    Set up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'http://yoursite.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Rails.application.secrets.email_domain,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_user_name,
+    password: Rails.application.secrets.email_password
+  }
 end
